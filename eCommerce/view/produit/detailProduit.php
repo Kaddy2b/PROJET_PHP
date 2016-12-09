bb1e8c2e267d6aa5a9aa8c65399ae1b2cf9bb4f8
Kaddy2b <gabriel2b20600@hotmail.fr>
2016-12-09 12:05:34 +0100

    correction detailProduit

--- eCommerce/view/produit/detailProduit.php
+++ eCommerce/view/produit/detailProduit.php
@@ -12,8 +12,8 @@
 			echo '<div><li>' . $v_prixProduit . ' €</li>';
 			echo '<li>' . $v_libProduit . '</li>';
 			echo '<li>En stock: ' . $v_stockProduit . '.</li>';
-			echo '<li><a href="./index.php?action=addPanier&id=' . $v_idProduit . '">Ajouter au Panier</a></li></div>'; 
-                        echo '<li><a href="./index.php?action=deleteProduit&id=' . $v_idProduit . '">Supprimer le produit</a></li></div>';
+			echo '<li><a href="./index.php?action=addPanier&id=' . $v_idProduit . '">Ajouter au Panier</a></li>'; 
+                        echo '<li><a href="./index.php?action=deleteProduit&id=' . $v_idProduit . '">Supprimer le produit</a></li>';
                         echo '<li><a href="./index.php?action=updateProduit&id=' . $v_idProduit . '">Modifier le produit</a></li></div>';
                         ?> 
                         

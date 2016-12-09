25853ca30b1a0831f878dbbc59cc942058a1c251
hugoblaco <hugo.blasco@yahoo.fr>
2016-12-09 12:38:27 +0100

    fixed list produit

--- eCommerce/view/produit/listProduit.php
+++ eCommerce/view/produit/listProduit.php
@@ -2,12 +2,11 @@
 	<h2>Tous les produits</h2>
 	<div class="mainList">
 		<?php
-                echo $tab_p;
 		foreach ($tab_p as $key => $value) { ?>
 		<ul class="containerProduit"> <?php
-                        
-                        $v_imageProduit = htmlspecialchars($p->getPhotoProduit());
-                        $v_idProduit = htmlspecialchars($p->getIdProduit());
+                        //$p inexistant, changer $p par $value car on doit accéder à l'objet de la classe, voir la condition de foreach plus haut
+                        $v_imageProduit = htmlspecialchars($value->getPhotoProduit());
+                        $v_idProduit = htmlspecialchars($value->getIdProduit());
 			echo "<li><img class=\"imageProduit\" src=\"$v_imageProduit\"></li>";
 			echo "<li><a href=\"./index.php?action=read&id=$v_idProduit\">> voir l'article</a></li>"; ?> 
 		</ul> <?php } ?>

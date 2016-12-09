23eacfc2956f4665a13c89d85103deef074e1a5a
Kaddy2b <gabriel2b20600@hotmail.fr>
2016-12-09 12:11:58 +0100

    blabla

--- eCommerce/view/produit/detailProduit.php
+++ eCommerce/view/produit/detailProduit.php
@@ -1,22 +1,22 @@
 <main>
 	<h2>Page du produit</h2>
 	<div class="mainDetail">
-		<ul> <?php
-                        $v_photoProduit = urlencode($p->getPhotoProduit());
-                        $v_prixProduit = urlencode($p->getPrixProduit());
-                        $v_libProduit = urlencode($p->getLibProduit());
-                        $v_stockProduit = urlencode($p->getStockProduit());
-                        $v_idProduit = urlencode($p->getIdProduit());
-                        //htmlspecialchars();
+		<ul>
+			<?php
+            $v_photoProduit = urlencode($p->getPhotoProduit());
+            $v_prixProduit = urlencode($p->getPrixProduit());
+            $v_libProduit = urlencode($p->getLibProduit());
+            $v_stockProduit = urlencode($p->getStockProduit());
+            $v_idProduit = urlencode($p->getIdProduit());
+            //htmlspecialchars();
 			echo '<li><img class="imageProduit" alt="photo du produit" src="' . $v_photoProduit . '"></li>';
 			echo '<div><li>' . $v_prixProduit . ' €</li>';
 			echo '<li>' . $v_libProduit . '</li>';
 			echo '<li>En stock: ' . $v_stockProduit . '.</li>';
 			echo '<li><a href="./index.php?action=addPanier&id=' . $v_idProduit . '">Ajouter au Panier</a></li>'; 
-                        echo '<li><a href="./index.php?action=deleteProduit&id=' . $v_idProduit . '">Supprimer le produit</a></li>';
-                        echo '<li><a href="./index.php?action=updateProduit&id=' . $v_idProduit . '">Modifier le produit</a></li></div>';
-                        ?> 
-                        
+            echo '<li><a href="./index.php?action=deleteProduit&id=' . $v_idProduit . '">Supprimer le produit</a></li>';
+            echo '<li><a href="./index.php?action=updateProduit&id=' . $v_idProduit . '">Modifier le produit</a></li></div>';
+            ?>             
 		</ul>
 	</div> 
 </main>

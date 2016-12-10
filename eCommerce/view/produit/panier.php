6fe72f5befd2c15814917a635fd9a3e2ce04a883
Kaddy2b <gabriel2b20600@hotmail.fr>
2016-12-11 00:01:22 +0100

    finalisation panier total + bouton commander

--- eCommerce/view/produit/panier.php
+++ eCommerce/view/produit/panier.php
@@ -4,7 +4,7 @@
 
 		<?php
 
-		var_dump($_SESSION['panier']);
+		//var_dump($_SESSION['panier']);
 
 		if(!isset($_SESSION['panier'])) {
 			echo "<h3>Votre panier est vide !</h3>";
@@ -12,7 +12,7 @@
 		else {
 			echo "<div class=\"barrePanier\"><span>Photo</span><span>Libellé</span><span>Quantité</span><span>Prix</span></div>";
 			$cpt = count($_SESSION['panier']);
-
+			$total = 0;
 			foreach ($_SESSION['panier'] as $key) {
 				$id = $key['id'];
 				$stock = $key['stock'];
@@ -23,7 +23,9 @@
 				echo "<span>" . $p->getLibProduit() . "</span>";
 				echo "<span>" . $stock . "<br><a href=\"./index.php?action=removeProduitPanier&id=" . $id . "\">-</a> | <a href=\"./index.php?action=addPanier&id=" . $id . "\">+</a></span>";
 				echo "<span>" . $p->getPrixProduit()*$stock . " €</span></div>";
+				$total = $total + ($p->getPrixProduit()*$stock);
 			}
+			echo '<span class="toMiddle">Total du panier: ' . $total . ' €. <a class="buttonCommander" href="#">Commander</a></span>';
 		}
 		?>
 

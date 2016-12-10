1b3a2b5927a80278c700552be23bd5ae0b09cfb2
Kaddy2b <gabriel2b20600@hotmail.fr>
2016-12-10 16:28:54 +0100

    caca

--- eCommerce/controller/routeur.php
+++ eCommerce/controller/routeur.php
@@ -32,47 +32,45 @@ else {
 
 
 //Vérifie tout les cas pour le controller
- if (isset($_GET['controller'])) {   // si le controller est mis dans l'url
-     
-     $controller = $_GET['controller'];
-    
-  }
-  else if(isset($_POST['controller'])){        //Si le controller est dans une URl
-       $controller = $_POST['controller'];
-  }
-  else{
-      $controller = "Produit";
-  }
+if (isset($_GET['controller'])) {   // si le controller est mis dans l'url
+$controller = $_GET['controller'];
+}
+else if(isset($_POST['controller'])){        //Si le controller est dans une URl
+  $controller = $_POST['controller'];
+}
+else{
+  $controller = "Produit";
+}
 
-  
+
   //Pareil pour l'action
  if (isset($_GET['action'])) {   // si l'action est mis dans l'url
-     $action = $_GET['action'];
- }
+ $action = $_GET['action'];
+}
  else if (isset($_POST['action'])){ // si l'action n'est pas dans un formulaire
-    $action = $_POST['action'];
- }
+ $action = $_POST['action'];
+}
    else{   //si l'action est transmis dans un formulaire
-          $action = "readAll";
+   $action = "readAll";
  }
-  
+
   $controller_class = "controller" . ucfirst($controller); //on concatene pour avoir le nom du controller final
 
   if(class_exists($controller_class)){ // Si le controller existe
        if(method_exists($controller_class,$action)){ // Si la méthode existe dans le controller
-           $controller_class::$action();
+         $controller_class::$action();
        }
        else{  //si la méthode n'existe pas dans le controller
            if($controller_class == "controllerProduit"){ //on configure de base un read all si le controller est Produit
-               $controller_class::readAll();
+             $controller_class::readAll();
            }
            else{
                echo "L'action effectuee n'existe pas";   //Message d'erreur à modifier avec un page erreur si on a le temps
+             }
            }
-       }
- } 
-  else{
+         } 
+         else{
      controllerProduit::readAll();   //de base on met un read all en produit
-  }
- 
-?>
+   }
+
+   ?>

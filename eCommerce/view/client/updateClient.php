8e5de56556e193b64af2ee422b4fd5c989b5ffb5
hugoblaco <hugo.blasco@yahoo.fr>
2016-12-13 11:24:03 +0100

    yolo presentation

--- eCommerce/view/client/updateClient.php
+++ eCommerce/view/client/updateClient.php
@@ -8,24 +8,13 @@
                 <input type="text" value="<?php echo $client->getId() ?>" name="nomClient" id="nom_id" readonly required/>
                 <br />
 
-                <label for="prenom_id">Prenom</label> :
-                <input type="text" value="Ex : Jacques" name="prenomClient" id="prenom_id" required/>
-                <br />
 
                 <label for="codePostal_id">Code Postal</label> :
-                <input type="text" value="Ex : 26 130" name="codePostalClient" id="codePostal_id" required/>
+                <input type="text" value= name="codePostalClient" id="codePostal_id" required/>
                 <br />
 
                 <label for="ville_id">Ville</label> :
                 <input type="text" value= name="villeClient" id="ville_id" required/>
-                <br />
-
-                <label for="email_id">Email</label> :
-                <input type="email" value="Ex : bonjour@exemple.com" name="emailClient" id="email_id" required/>
-                <br />
-
-                <label for="login_id">login</label> :
-                <input type="text" value="Ex : Amn3si" name="loginClient" id="login_id" required/>
                 <br />
 
                 <label for="mdp_id">Mot de passe</label> :
